0810793c80e19b3cd2d02d030c88a4abbf8b7ea1
Armando Bibay <armandobibayjr@Armandos-MacBook-Pro.local>
2021-06-08 21:05:45 +0800

    where clause

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const expressHandlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -11,12 +12,14 @@ const app = express();
 
 authenticateDatabase();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, console.log(`server running on port ${PORT}`));
+app.use("/gigs", require("./src/routes/gigs.routes"));
 app.get("/", (req, res) => {
   res.send("INDEX");
 });
-
-app.use("/gigs", require("./src/routes/gigs.routes"));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`server running on port ${PORT}`));
--- src/routes/gigs.routes.js
+++ src/routes/gigs.routes.js
@@ -13,13 +13,29 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/getUserById", async (req, res) => {
+  const queryId = req.query.id;
+  await Gig.findAll({
+    where: {
+      id: queryId,
+    },
+  })
+    .then((gig) => {
+      res.send(gig).sendStatus(200);
+    })
+    .catch((err) => {
+      res.send(err).sendStatus(500);
+    });
+});
+
 router.post("/create", (req, res) => {
+  console.log(req.body);
   Gig.create({
-    title: "title",
-    technologies: "C#",
-    description: "sample desc",
-    budget: "P100",
-    contact_email: "hello@example.com",
+    title: req.body.title,
+    technologies: req.body.technologies,
+    description: req.body.description,
+    budget: req.body.budget,
+    contact_email: req.body.contact_email,
   })
     .then((gig) => {
       res.sendStatus(200);
